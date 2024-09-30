d42a997d1cd9e9852943bbd4cca6b2ac3ef2eb04
fraanfx <fran.am95@gmail.com>
2024-09-30 11:10:25 +0200

    Finished exercise

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
+use std::result;
+
 fn main() {
-    println!("Hello, world!");
+    let string_one = String::from("Hello");
+    let string_two = String::from(" world");
+    concatenate_strings(string_one, string_two);
 }
 
-fn concatenate_strings(str1: &str, str2: &str){
-    println!("{}", str1);
+fn concatenate_strings(str1: String, str2: String) -> String{
+    let mut result = str1;
+    result.push_str(&str2);
+    println!("Result: {}", result);
+    result
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concatenate_strings() {
+        let string_one = String::from("Hello");
+        let string_two = String::from(" world");
+        let result = concatenate_strings(string_one, string_two);
+        assert_eq!(result, String::from("Hello world"));
+    }
+}
+
+
+
